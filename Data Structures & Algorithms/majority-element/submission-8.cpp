dca905f1c2e036cb2c4c9144c6078833d7f1ff10
Asim Ahmad Siddiqui <156048782+asim-sidiqi@users.noreply.github.com>
2026-04-04 21:51:34 +0530

    Add: majority-element - submission-8

---
+++ Data Structures & Algorithms/majority-element/submission-8.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int elem;
+        int count=0;
+        int n = nums.size();
+        for(int i=0; i<n; i++){
+            if(count==0){
+                count = 1;
+                elem = nums[i];
+            }
+            else if(nums[i]==elem) count++;
+            else count--;
+        }
+
+        //counting again to verify
+        count=0;
+        for(int i=0; i<n; i++){
+            if(nums[i]==elem) count++;
+        }
+
+        if(count<=n/2) return -1;
+
+        return elem;
+    }
+};
