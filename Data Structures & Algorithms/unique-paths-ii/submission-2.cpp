bfc02667cc24b9415468d7ce71a4783605cc7514
Asim Ahmad Siddiqui <156048782+asim-sidiqi@users.noreply.github.com>
2026-04-10 16:48:03 +0530

    Add: unique-paths-ii - submission-2

---
+++ Data Structures & Algorithms/unique-paths-ii/submission-2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+private:
+    int solve(int m, int n, vector<vector<int>>& grid, vector<vector<int>>& dp){
+        if(m<0 || n<0) return 0;
+        if(grid[m][n]==1) return 0;
+        if(m==0 && n==0) return 1;
+
+        if(dp[m][n]!=-1) return dp[m][n];
+
+        int l = solve(m-1,n,grid, dp);
+        int r = solve(m,n-1,grid, dp);
+
+        return dp[m][n] = l+r;
+    }
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n,-1));
+
+        return solve(m-1, n-1, obstacleGrid, dp);
+    }
+};
