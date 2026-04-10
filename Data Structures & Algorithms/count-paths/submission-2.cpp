a62f5f48f250852784777fd1d9aba057702cc787
Asim Ahmad Siddiqui <156048782+asim-sidiqi@users.noreply.github.com>
2026-04-10 15:56:29 +0530

    Add: count-paths - submission-2

---
+++ Data Structures & Algorithms/count-paths/submission-2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+private:
+    int solve(int m, int n, vector<vector<int>>& dp){
+        if(m==0 && n==0) return 1;
+        if(m<0 || n<0) return 0;
+
+        if(dp[m][n]!=-1) return dp[m][n];
+
+        int l = solve(m-1,n, dp);
+        int r = solve(m,n-1, dp);
+
+        return dp[m][n] = l+r;
+    }
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n,-1));
+        return solve(m-1, n-1, dp);
+    }
+};
