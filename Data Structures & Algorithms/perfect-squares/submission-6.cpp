c3f3e2d1ff76a11d0d16e0c8dce6cb7aa69e2955
Asim Ahmad Siddiqui <156048782+asim-sidiqi@users.noreply.github.com>
2026-07-14 19:46:23 +0530

    Update: perfect-squares - submission-6

--- Data Structures & Algorithms/perfect-squares/submission-6.cpp
+++ Data Structures & Algorithms/perfect-squares/submission-6.cpp
@@ -15,16 +15,7 @@ private:
     }
 public:
     int numSquares(int n) {
-        vector<int> dp(n+1, INT_MAX);
-        //return solve(n,dp);
-
-        dp[0]=0;
-        for(int i=1; i<=n; i++){
-            for(int j=1; j*j<=i; j++){
-                dp[i] = min(dp[i], 1+dp[i-j*j]);
-            }
-        }
-
-        return dp[n];
+        vector<int> dp(n+1, -1);
+        return solve(n,dp);
     }
 };
