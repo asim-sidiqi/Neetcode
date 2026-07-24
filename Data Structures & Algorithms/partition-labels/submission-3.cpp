ef8663c983ba19533dc1b80918254e3a4b17f244
Asim Ahmad Siddiqui <156048782+asim-sidiqi@users.noreply.github.com>
2026-07-24 23:08:05 +0530

    Add: partition-labels - submission-3

---
+++ Data Structures & Algorithms/partition-labels/submission-3.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        int n = s.size();
+        vector<int> ans;
+        unordered_map<char,int> m;
+        for(int i=0; i<n; i++){
+            m[s[i]] = i;
+        }
+
+        int last = 0;
+        for(int i=0; i<n; i++){
+            last = m[s[i]];
+            cout<<last<<endl;
+            for(int j=i; j<=last; j++){
+                last = max(last,m[s[j]]);
+            }
+
+            ans.push_back(last-i+1);
+            i=last;
+        }
+
+        return ans;
+    }
+};
