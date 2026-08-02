b8cc1304a709257069906db10222ba9c7fa0b2e6
Asim Ahmad Siddiqui <156048782+asim-sidiqi@users.noreply.github.com>
2026-08-02 21:18:08 +0530

    Add: interleaving-string - submission-0

---
+++ Data Structures & Algorithms/interleaving-string/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+private:
+    bool solve(int i, int j, string &s1, string &s2, string &s3) {
+        if (i == s1.size() && j == s2.size())
+            return true;
+
+        bool ans = false;
+
+        if (i < s1.size() && s1[i] == s3[i + j])
+            ans |= solve(i + 1, j, s1, s2, s3);
+
+        if (j < s2.size() && s2[j] == s3[i + j])
+            ans |= solve(i, j + 1, s1, s2, s3);
+
+        return ans;
+    }
+
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        if (s1.size() + s2.size() != s3.size())
+            return false;
+
+        return solve(0, 0, s1, s2, s3);
+    }
+};
