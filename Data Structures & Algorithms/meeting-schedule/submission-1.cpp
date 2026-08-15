289b3a463935398651f4d35e1a46fa3e8af729fd
Asim Ahmad Siddiqui <156048782+asim-sidiqi@users.noreply.github.com>
2026-08-15 22:26:22 +0530

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        int n = intervals.size();
+        sort(intervals.begin(),intervals.end(), [](Interval a, Interval b){
+            if(a.start==b.start) return a.end<b.end;
+            else return a.start<b.start;
+        });
+
+        for(int i=0; i<n-1; i++){
+            if(intervals[i].end>intervals[i+1].start) return false;
+        }
+
+        return true;
+    }
+};
