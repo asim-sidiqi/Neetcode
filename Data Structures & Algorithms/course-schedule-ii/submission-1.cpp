ab88c8491dbf9b4b046432952d5b59310858fae0
Asim Ahmad Siddiqui <156048782+asim-sidiqi@users.noreply.github.com>
2026-09-12 23:40:15 +0530

    Add: course-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-1.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int,vector<int>> m;
+        for(auto it:prerequisites){
+            int u = it[0];
+            int v = it[1];
+
+            m[v].push_back(u);
+        }
+
+        vector<int> indegree(numCourses,0);
+        for(int i=0; i<numCourses; i++){
+            for(auto it:m[i]){
+                indegree[it]++;
+            }
+        }
+
+        queue<int> q;
+        for(int i=0; i<indegree.size(); i++){
+            if(indegree[i]==0) q.push(i);
+        }
+
+        vector<int> ans;
+        int count=0;
+        while(!q.empty()){
+            int temp = q.front();
+            q.pop();
+
+            ans.push_back(temp);
+            count++;
+
+            for(auto it:m[temp]){
+                indegree[it]--;
+                if(indegree[it]==0) q.push(it);
+            }
+        }
+
+        if(count<numCourses) return {};
+        return ans;
+    }
+};
